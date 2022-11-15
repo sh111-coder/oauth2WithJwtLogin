0559f9dcfe031fb3e66ca250b2c0ddfbf17fb465
ksh-beginner <ohk9134@naver.com>
2022-11-15 21:22:31 +0900

    feat : JWT 인증 필터 인증 메소드에서 소셜 로그인 유저 임의 비밀번호 설정하여 인증 통과하도록 설정

--- src/main/java/login/oauthtest4/global/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ src/main/java/login/oauthtest4/global/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -3,7 +3,9 @@ package login.oauthtest4.global.jwt.filter;
 import login.oauthtest4.domain.user.User;
 import login.oauthtest4.domain.user.repository.UserRepository;
 import login.oauthtest4.global.jwt.service.JwtService;
+import login.oauthtest4.global.oauth2.util.PasswordUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
@@ -18,6 +20,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  * Jwt 인증 필터
@@ -28,6 +31,7 @@ import java.io.IOException;
  * "/login" 이외의 URI 요청이 왔을 때 처리하는 필터
  */
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 
     private static final String NO_CHECK_URL = "/login"; // "/login"으로 들어오는 요청은 Filter 작동 X
@@ -89,6 +93,7 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
      */
     public void checkAccessTokenAndAuthentication(HttpServletRequest request, HttpServletResponse response,
                                                   FilterChain filterChain) throws ServletException, IOException {
+        log.info("checkAccessTokenAndAuthentication() 호출");
         jwtService.extractAccessToken(request)
                 .filter(jwtService::isTokenValid)
                 .ifPresent(accessToken -> jwtService.extractEmail(accessToken)
@@ -110,14 +115,18 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
      * UserDetails의 User 객체 안에 Set<GrantedAuthority> authorities이 있어서 getter로 호출한 후에,
      * new NullAuthoritiesMapper()로 GrantedAuthoritiesMapper 객체를 생성하고 mapAuthorities()에 담기
      *
-     * SecurityContextHolder.createEmptyContext()로 빈 SecurityContext 객체를 생성한 후
+     * SecurityContextHolder.getContext()로 SecurityContext를 꺼낸 후,
      * setAuthentication()을 이용하여 위에서 만든 Authentication 객체에 대한 인증 허가 처리
-     * 그 후 인증 허가 처리된 객체를 setContext()를 통해 SecurityContextHolder 에 담기
      */
     public void saveAuthentication(User myUser) {
+        String password = myUser.getPassword();
+        if (password == null) { // 소셜 로그인 유저의 비밀번호 임의로 설정 하여 소셜 로그인 유저도 인증 되도록 설정
+            password = PasswordUtil.generateRandomPassword();
+        }
+
         UserDetails userDetailsUser = org.springframework.security.core.userdetails.User.builder()
                 .username(myUser.getEmail())
-                .password(myUser.getPassword())
+                .password(password)
                 .roles(myUser.getRole().name())
                 .build();
 
@@ -125,8 +134,6 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
                 new UsernamePasswordAuthenticationToken(userDetailsUser, null,
                 authoritiesMapper.mapAuthorities(userDetailsUser.getAuthorities()));
 
-        SecurityContext context = SecurityContextHolder.createEmptyContext();
-        context.setAuthentication(authentication);
-        SecurityContextHolder.setContext(context);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
---
+++ src/main/java/login/oauthtest4/global/oauth2/util/PasswordUtil.java
@@ -0,0 +1,30 @@
+package login.oauthtest4.global.oauth2.util;
+
+import java.util.Random;
+
+public class PasswordUtil {
+
+    public static String generateRandomPassword() {
+        int index = 0;
+        char[] charSet = new char[] {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };	//배열안의 문자 숫자는 원하는대로
+
+        StringBuffer password = new StringBuffer();
+        Random random = new Random();
+
+        for (int i = 0; i < 8 ; i++) {
+            double rd = random.nextDouble();
+            index = (int) (charSet.length * rd);
+
+            password.append(charSet[index]);
+        }
+        System.out.println(password);
+        return password.toString();
+        //StringBuffer를 String으로 변환해서 return 하려면 toString()을 사용하면 된다.
+    }
+}
