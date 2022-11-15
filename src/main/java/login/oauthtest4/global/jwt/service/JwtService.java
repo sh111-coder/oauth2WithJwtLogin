7d11e988ac9b38825417f70b24230f5980ddcf7f
ksh-beginner <ohk9134@naver.com>
2022-11-15 21:21:13 +0900

    fix(JwtService) : 토큰 생성 알고리즘 HMAC512로 통일

--- src/main/java/login/oauthtest4/global/jwt/service/JwtService.java
+++ src/main/java/login/oauthtest4/global/jwt/service/JwtService.java
@@ -134,7 +134,7 @@ public class JwtService {
     public Optional<String> extractEmail(String accessToken) {
         try {
             // 토큰 유효성 검사하는 데에 사용할 알고리즘이 있는 JWT verifier builder 반환
-            return Optional.ofNullable(JWT.require(Algorithm.HMAC256(secretKey))
+            return Optional.ofNullable(JWT.require(Algorithm.HMAC512(secretKey))
                     .build() // 반환된 빌더로 JWT verifier 생성
                     .verify(accessToken) // accessToken을 검증하고 유효하지 않다면 예외 발생
                     .getClaim(EMAIL_CLAIM) // claim(Emial) 가져오기
@@ -172,10 +172,10 @@ public class JwtService {
 
     public boolean isTokenValid(String token) {
         try {
-            JWT.require(Algorithm.HMAC256(secretKey)).build().verify(token);
+            JWT.require(Algorithm.HMAC512(secretKey)).build().verify(token);
             return true;
         } catch (Exception e) {
-            log.error("유효하지 않은 토큰입니다.", e.getMessage());
+            log.error("유효하지 않은 토큰입니다. {}", e.getMessage());
             return false;
         }
     }
