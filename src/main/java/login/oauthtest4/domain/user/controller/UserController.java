abd4d343d03860ff7d89cfc6ff2cc2cb13cd4da3
ksh-beginner <ohk9134@naver.com>
2022-11-15 21:23:46 +0900

    feat(UserController) : 테스트용 API 추가

--- src/main/java/login/oauthtest4/domain/user/controller/UserController.java
+++ src/main/java/login/oauthtest4/domain/user/controller/UserController.java
@@ -3,6 +3,7 @@ package login.oauthtest4.domain.user.controller;
 import login.oauthtest4.domain.user.dto.UserSignUpDto;
 import login.oauthtest4.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,10 @@ public class UserController {
     public String signUp(@RequestBody UserSignUpDto userSignUpDto) throws Exception {
         userService.signUp(userSignUpDto);
         return "회원가입 성공";
+    }
+
+    @GetMapping("/jwt-test")
+    public String jwtTest() {
+        return "jwtTest 요청 성공";
     }
 }
