8b4056d80dfa87185c35b273f35a896fbdacad52
ksh-beginner <ohk9134@naver.com>
2022-11-26 23:38:21 +0900

    refactor : 로그인 성공 시 리다이렉트 링크 URL oauth2 추가
    로그인 성공 시 리다이렉트는 OAuth2 로그인 한 유저들만 리다이렉트 되므로
    URL에 oauth2 추가

--- src/main/java/login/oauthtest4/global/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/login/oauthtest4/global/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -31,7 +31,7 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
             if(oAuth2User.getRole() == Role.GUEST) {
                 String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
                 response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
-                response.sendRedirect("/sign-up"); // 프론트의 회원가입 추가 폼으로 리다이렉트
+                response.sendRedirect("oauth2/sign-up"); // 프론트의 회원가입 추가 정보 입력 폼으로 리다이렉트
 
                 jwtService.sendAccessAndRefreshToken(response, accessToken, null);
             } else {
