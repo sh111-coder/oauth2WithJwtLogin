fece58e5490cc87e18c4f53242ce939b8b250668
ksh-beginner <ohk9134@naver.com>
2022-11-27 12:04:36 +0900

    fix : 로그인 실패 시 Response Body 메시지 한글 깨지지 않도록 수정

--- src/main/java/login/oauthtest4/global/login/handler/LoginFailureHandler.java
+++ src/main/java/login/oauthtest4/global/login/handler/LoginFailureHandler.java
@@ -19,6 +19,8 @@ public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException exception) throws IOException {
         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/plain;charset=UTF-8");
         response.getWriter().write("로그인 실패! 이메일이나 비밀번호를 확인해주세요.");
         log.info("로그인에 실패했습니다. 메시지 : {}", exception.getMessage());
     }
