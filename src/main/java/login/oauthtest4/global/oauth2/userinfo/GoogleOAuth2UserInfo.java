0887daa84eee32c5cd0a3651636f929d67dddb40
ksh-beginner <ohk9134@naver.com>
2023-01-04 15:13:11 +0900

    style(OAuth2UserInfo) : 스타일 정리

--- src/main/java/login/oauthtest4/global/oauth2/userinfo/GoogleOAuth2UserInfo.java
+++ src/main/java/login/oauthtest4/global/oauth2/userinfo/GoogleOAuth2UserInfo.java
@@ -1,10 +1,7 @@
 package login.oauthtest4.global.oauth2.userinfo;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.Map;
 
-@Slf4j
 public class GoogleOAuth2UserInfo extends OAuth2UserInfo {
 
     public GoogleOAuth2UserInfo(Map<String, Object> attributes) {
--- src/main/java/login/oauthtest4/global/oauth2/userinfo/KakaoOAuth2UserInfo.java
+++ src/main/java/login/oauthtest4/global/oauth2/userinfo/KakaoOAuth2UserInfo.java
@@ -4,8 +4,6 @@ import java.util.Map;
 
 public class KakaoOAuth2UserInfo extends OAuth2UserInfo {
 
-
-
     public KakaoOAuth2UserInfo(Map<String, Object> attributes) {
         super(attributes);
     }
