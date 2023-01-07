179e5230dfc0977523b56b3080f1786a17fa3c8b
ksh-beginner <ohk9134@naver.com>
2023-01-07 11:54:32 +0900

    feat(CustomOAuth2UserService) : 불필요한 GOOGLE 상수 삭제

--- src/main/java/login/oauthtest4/global/oauth2/service/CustomOAuth2UserService.java
+++ src/main/java/login/oauthtest4/global/oauth2/service/CustomOAuth2UserService.java
@@ -27,7 +27,6 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     private static final String NAVER = "naver";
     private static final String KAKAO = "kakao";
-    private static final String GOOGLE = "google";
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
